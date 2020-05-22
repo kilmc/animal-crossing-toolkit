95b29898117ec92474dfabd0324c6b70c2229184
Kilian McMahon <kil@kilmc.com>
2020-05-22 19:47:21 -0400

    Rework page structure and add footer

--- src/App.tsx
+++ src/App.tsx
@@ -93,38 +93,41 @@ function App() {
   return (
     <FilterContext.Provider value={{ state, setState }}>
       <GoogleSheetsProvider>
-        <div>
-          <div className="bg-yellow-300 layout-main">
-            <header className="sticky t0 z1 px3x flex h8x bg-brown-900 text-cream-200 items-center">
-              <nav className="self-end">
-                <button className="bg-cream-200 text-brown-800 radius-top1x text-bold p1x fz16px">
-                  Critter Helper
-                </button>
-              </nav>
+        <div className="bg-yellow-300">
+          <header className="layout-header sticky t0 z1">
+            <nav className="px3x flex h8x bg-brown-900 text-cream-200 items-center">
+              <button className="z2 self-end bg-cream-200 text-brown-800 radius-top1x text-bold p1x fz16px">
+                Critter Helper
+              </button>
               <h1 className="fz20px ml-auto">ACNH Toolkit</h1>
-            </header>
-            <div className="layout-body">
-              <div>
-                <CritterAppSettings
-                  settingsOpen={state.settingsOpen}
-                  setSettingsVisibility={setSettingsVisibility}
-                  showDonated={state.showDonated}
-                  setShowDonated={setShowDonated}
-                  showInactive={state.showInactive}
-                  setShowInactive={setShowInactive}
-                  hemisphere={state.hemisphere}
-                  setHemisphere={setHemisphere}
-                  critterType={state.critterType}
-                  setCritter={setCritter}
-                  donationsCount={{
-                    bugs: donatedBugsCount,
-                    fish: donatedFishCount,
-                  }}
-                />
-              </div>
-              <BugsList />
-            </div>
-          </div>
+            </nav>
+
+            <CritterAppSettings
+              settingsOpen={state.settingsOpen}
+              setSettingsVisibility={setSettingsVisibility}
+              showDonated={state.showDonated}
+              setShowDonated={setShowDonated}
+              showInactive={state.showInactive}
+              setShowInactive={setShowInactive}
+              hemisphere={state.hemisphere}
+              setHemisphere={setHemisphere}
+              critterType={state.critterType}
+              setCritter={setCritter}
+              donationsCount={{
+                bugs: donatedBugsCount,
+                fish: donatedFishCount,
+              }}
+            />
+          </header>
+
+          <BugsList />
+
+          <footer className="text-center p10vh text-brown-800">
+            Made with love by{" "}
+            <a className="text-brown-800" href="https://kilian.website">
+              Kilian
+            </a>
+          </footer>
         </div>
       </GoogleSheetsProvider>
     </FilterContext.Provider>
